2186367de01ecf76ec017988bcc8796d77674ec3
Noah <bantucaravan@gmail.com>
2020-01-09 00:51:39 -0500

    simplify allcol, add allrow

--- noahs_utility_funcs.py
+++ noahs_utility_funcs.py
@@ -7,12 +7,15 @@ import pandas as pd
 # fix 8 space default
 
 def allcols(df, rows=False):
-    if not rows:
-        with pd.option_context('display.max_columns', df.shape[-1]):
-            display(df)
+    with pd.option_context('display.max_columns', df.shape[-1]):
+        display(df)
 
-    if rows:
-        with pd.option_context('display.max_columns', df.shape[-1], 'display.max_rows', df.shape[0]):
-            display(df)
+def allrows(df):
+    with pd.option_context('display.max_rows', None):#len(df)):
+        display(df)
+
+def allcolsrows(df):
+    with pd.option_context('display.max_columns', df.shape[-1], 'display.max_rows', df.shape[0]):
+        display(df)
 
 
