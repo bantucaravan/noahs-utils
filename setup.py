3d2d46c94bcd1cffaf5dcc27c7d1005361d4302d
noah <noah.chasek-macfoy@ibm.com@noahs-MacBook-Pro.local>
2020-11-27 00:27:27 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name="noahs"
+    version="0.0.1",
+    author="Noah Chasek-Macfoy",
+    author_email="bantucaravan@gmail.com",
+    description="Data Science tools I like to use.",
+    #url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        #"License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
