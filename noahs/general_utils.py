bafcdd7ea12402acb000de3f5d40b57e9c7a46c2
Noah Chasek-Macfoy <bantucaravan@gmail.com>
2024-02-22 15:55:42 -0700

    update pandas json_normalize import

--- noahs/general_utils.py
+++ noahs/general_utils.py
@@ -1,10 +1,13 @@
-
+y
 
 from IPython.display import display
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from pandas.io.json import json_normalize
+if pd.__version__ >= '2':
+    json_normalize = pd.json_normalize 
+else:
+    from pandas.io.json import json_normalize
 from sklearn.metrics import classification_report, roc_curve, roc_auc_score, accuracy_score, r2_score, mean_absolute_error
 from sklearn.metrics import confusion_matrix as cm_sklearn
 import scipy.stats
